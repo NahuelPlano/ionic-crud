147930eca484bc506e9f1bf84047ab2b69efedb9
NahuelPlano <65472800+NahuelPlano@users.noreply.github.com>
2021-12-12 21:56:57 -0300

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -1,7 +1,7 @@
 <?php
 
 
-
+header('Access-Control-Allow-Origin, *');
 header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, OPTIONS');
 header('Access-Control-Allow-Headers: token, Content-Type');
 header('Access-Control-Allow-Max-Age: 1728000');
